3b4f17bc8a3591836b918643cec583a8273a1536
morphyyyy <morphysmate@hotmail.com>
2023-09-19 19:33:48 -0500

    Initial commit!

---
+++ Madden-CustomPlaybook-Editor/frmEditFormation.cs
@@ -0,0 +1,407 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Madden_CustomPlaybook_Editor
+{
+    public partial class frmEditFormation : Form
+    {
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private frmPlaybook playbookFormRef;
+        public List<SETL> DefaultSETL;
+        public List<SETP> DefaultSETP;
+        public List<SETG> DefaultSETG;
+
+        public frmEditFormation(frmPlaybook playbookFormHandle)
+        {
+            playbookFormRef = playbookFormHandle;
+            InitializeComponent();
+            AddDrag(mnuPBPL);
+        }
+
+        private void frmEditFormation_Load(object sender, EventArgs e)
+        {
+            playbookFormRef.cbxFormations.Enabled = false;
+            playbookFormRef.cbxSubFormations.Enabled = false;
+            playbookFormRef.cbxAlignments.Enabled = false;
+            playbookFormRef.btnEditFormation.Enabled = false;
+            playbookFormRef.btnEditPlayData.Enabled = false;
+            playbookFormRef.chbFlipPlay.Enabled = false;
+
+            setSETL();
+            setSETP();
+            setSETG();
+        }
+
+        private void frmEditFormation_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            playbookFormRef.cbxFormations.Enabled = true;
+            playbookFormRef.cbxSubFormations.Enabled = true;
+            playbookFormRef.btnEditFormation.Enabled = true;
+            playbookFormRef.btnEditPlayData.Enabled = true;
+            if (playbookFormRef.mnuOptions_Playart_PSAL.Checked)
+            {
+                playbookFormRef.chbFlipPlay.Enabled = true;
+                playbookFormRef.cbxAlignments.Enabled = true;
+            }
+            else
+            {
+                playbookFormRef.chbFlipPlay.Enabled = false;
+                playbookFormRef.cbxAlignments.Enabled = false;
+            }
+        }
+
+        #region Title Bar
+
+        private void btn_Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_Close_MouseEnter(object sender, EventArgs e)
+        {
+            btn_Close.Image = ((Image)(Properties.Resources.btn_close_hover));
+        }
+
+        private void btn_Close_MouseLeave(object sender, EventArgs e)
+        {
+            btn_Close.Image = ((Image)(Properties.Resources.btn_close));
+        }
+
+        private void AddDrag(Control Control)
+        {
+            Control.MouseDown += new MouseEventHandler(DragForm_MouseDown);
+        }
+
+        private void DragForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        #endregion
+
+        private void btnResetSETL_Click(object sender, EventArgs e)
+        {
+            playbookFormRef.Play.SETL = (from row in DefaultSETL
+                                         select new SETL()
+                                         {
+                                             rec = row.rec,
+                                             setl = row.setl,
+                                             FORM = row.FORM,
+                                             MOTN = row.MOTN,
+                                             CLAS = row.CLAS,
+                                             SETT = row.SETT,
+                                             SITT = row.SITT,
+                                             SLF_ = row.SLF_,
+                                             name = row.name,
+                                             poso = row.poso
+                                         }).Cast<SETL>().ToList();
+
+            dgvSETL.AutoResizeColumns();
+            int SETLrec = ((SETL)playbookFormRef.cbxSubFormations.SelectedItem).rec;
+            int PBPLrec = ((PBPL)playbookFormRef.cbxPlays.SelectedItem).rec;
+
+            playbookFormRef.SetPlay(playbookFormRef.Play);
+            playbookFormRef.Play = playbookFormRef.GetPlay();
+            playbookFormRef.UpdateSubFormations();
+            playbookFormRef.UpdateAlignments();
+            playbookFormRef.UpdatePlays();
+
+            playbookFormRef.cbxSubFormations.SelectedItem = (playbookFormRef.cbxSubFormations.Items.Cast<SETL>().ToList()).Find(item => item.rec == SETLrec);
+            playbookFormRef.cbxPlays.SelectedItem = (playbookFormRef.cbxPlays.Items.Cast<PBPL>().ToList()).Find(item => item.rec == PBPLrec);
+
+            playbookFormRef.UpdatePlay(playbookFormRef.Play);
+            dgvSETL.DataSource = playbookFormRef.Play.SETL;
+
+            Program.ResizeDataGrid(dgvSETL);
+            Focus();
+        }
+
+        private void btnResetSETP_Click(object sender, EventArgs e)
+        {
+            playbookFormRef.Play.SETP = (from row in DefaultSETP select new SETP()
+            {
+                rec = row.rec,
+                SETL = row.SETL,
+                setp = row.setp,
+                SGT_ = row.SGT_,
+                arti = row.arti,
+                opnm = row.opnm,
+                tabo = row.tabo,
+                poso = row.poso,
+                odep = row.odep,
+                flas = row.flas,
+                DPos = row.DPos,
+                EPos = row.EPos,
+                fmtx = row.fmtx,
+                artx = row.artx,
+                fmty = row.fmty,
+                arty = row.arty
+            }).Cast<SETP>().ToList();
+            playbookFormRef.SetPlay(playbookFormRef.Play);
+            playbookFormRef.Play = playbookFormRef.GetPlay();
+            playbookFormRef.UpdatePlay(playbookFormRef.Play);
+            dgvSETP.DataSource = playbookFormRef.Play.SETP;
+
+            Program.ResizeDataGrid(dgvSETP);
+            Focus();
+        }
+
+        private void btnResetSETG_Click(object sender, EventArgs e)
+        {
+            playbookFormRef.SETG = (from row in DefaultSETG select new SETG()
+            {
+                rec = row.rec,
+                SETP = row.SETP,
+                SGF_ = row.SGF_,
+                SF__ = row.SF__,
+                x___ = row.x___,
+                y___ = row.y___,
+                fx__ = row.fx__,
+                fy__ = row.fy__,
+                anm_ = row.anm_,
+                dir_ = row.dir_,
+                fanm = row.fanm,
+                fdir = row.fdir,
+                active = row.active
+            }).Cast<SETG>().ToList();
+            playbookFormRef.Play = playbookFormRef.GetPlay();
+            playbookFormRef.UpdatePlay(playbookFormRef.Play);
+            setSETG();
+            Focus();
+        }
+
+        private void setSETL()
+        {
+            dgvSETL.DataSource = playbookFormRef.Play.SETL;
+
+            dgvSETL.Columns["rec"].ReadOnly = true;
+            dgvSETL.Columns["SETL"].ReadOnly = true;
+
+            dgvSETL.AutoResizeColumns();
+            Program.ResizeDataGrid(dgvSETL);
+
+            foreach (SETL setl in playbookFormRef.Play.SETL)
+            {
+                Console.WriteLine(setl);
+            }
+        }
+
+        private void dgvSETL_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            dgvSETL.AutoResizeColumns();
+            Program.ResizeDataGrid(dgvSETL);
+            int SETLrec = ((SETL)playbookFormRef.cbxSubFormations.SelectedItem).rec;
+            int PBPLrec = ((PBPL)playbookFormRef.cbxPlays.SelectedItem).rec;
+
+            playbookFormRef.SetPlay(playbookFormRef.Play);
+            playbookFormRef.Play = playbookFormRef.GetPlay();
+            playbookFormRef.UpdateSubFormations();
+            playbookFormRef.UpdateAlignments();
+            playbookFormRef.UpdatePlays();
+
+            playbookFormRef.cbxSubFormations.SelectedItem = (playbookFormRef.cbxSubFormations.Items.Cast<SETL>().ToList()).Find(item => item.rec == SETLrec);
+            playbookFormRef.cbxPlays.SelectedItem = (playbookFormRef.cbxPlays.Items.Cast<PBPL>().ToList()).Find(item => item.rec == PBPLrec);
+
+            playbookFormRef.UpdatePlay(playbookFormRef.Play);
+            setSETL();
+
+            Focus();
+        }
+
+        private void setSETP()
+        {
+            dgvSETP.DataSource = playbookFormRef.Play.SETP;
+
+            dgvSETP.Columns["rec"].ReadOnly = true;
+            dgvSETP.Columns["SETL"].ReadOnly = true;
+            dgvSETP.Columns["SETP"].ReadOnly = true;
+            dgvSETP.Columns["poso"].ReadOnly = true;
+
+            dgvSETP.AutoResizeColumns();
+            Program.ResizeDataGrid(dgvSETP);
+
+            foreach (SETP setp in playbookFormRef.Play.SETP)
+            {
+                Console.WriteLine(setp);
+            }
+        }
+
+        private void dgvSETP_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            playbookFormRef.SetPlay(playbookFormRef.Play);
+            playbookFormRef.Play = playbookFormRef.GetPlay();
+            playbookFormRef.UpdatePlay(playbookFormRef.Play);
+            setSETP();
+
+            Program.ResizeDataGrid(dgvSETP);
+            Focus();
+        }
+
+        private void setSETG()
+        {
+            dgvSETG.DataSource = playbookFormRef.Play.SETG;
+
+            dgvSETG.Columns["active"].Visible = false;
+            dgvSETG.Columns["rec"].ReadOnly = true;
+            dgvSETG.Columns["rec"].DefaultCellStyle.BackColor = Color.White;
+            dgvSETG.Columns["rec"].DefaultCellStyle.SelectionBackColor = Color.White;
+            dgvSETG.Columns["rec"].DefaultCellStyle.ForeColor = Color.DarkGray;
+            dgvSETG.Columns["rec"].DefaultCellStyle.SelectionForeColor = Color.DarkGray;
+            dgvSETG.Columns["SETP"].ReadOnly = true;
+            dgvSETG.Columns["SETP"].ReadOnly = true;
+            dgvSETG.Columns["SETP"].DefaultCellStyle.BackColor = Color.White;
+            dgvSETG.Columns["SETP"].DefaultCellStyle.SelectionBackColor = Color.White;
+            dgvSETG.Columns["SETP"].DefaultCellStyle.ForeColor = Color.DarkGray;
+            dgvSETG.Columns["SETP"].DefaultCellStyle.SelectionForeColor = Color.DarkGray;
+            dgvSETG.Columns["SGF_"].ReadOnly = true;
+            dgvSETG.Columns["SGF_"].ReadOnly = true;
+            dgvSETG.Columns["SGF_"].DefaultCellStyle.BackColor = Color.White;
+            dgvSETG.Columns["SGF_"].DefaultCellStyle.SelectionBackColor = Color.White;
+            dgvSETG.Columns["SGF_"].DefaultCellStyle.ForeColor = Color.DarkGray;
+            dgvSETG.Columns["SGF_"].DefaultCellStyle.SelectionForeColor = Color.DarkGray;
+
+            dgvSETG.AutoResizeColumns();
+            Program.ResizeDataGrid(dgvSETG);
+
+            for (int i = 0; i < playbookFormRef.Play.SETG.Count; i++)
+            {
+                if (!(playbookFormRef.Play.SETG[i].active))
+                {
+                    dgvSETG.Rows[i].ReadOnly = true;
+                    dgvSETG.Rows[i].DefaultCellStyle.BackColor = Color.White;
+                    dgvSETG.Rows[i].DefaultCellStyle.SelectionBackColor = Color.White;
+                    dgvSETG.Rows[i].DefaultCellStyle.ForeColor = Color.DarkGray;
+                    dgvSETG.Rows[i].DefaultCellStyle.SelectionForeColor = Color.DarkGray;
+                }
+            }
+
+            foreach (SETG setg in playbookFormRef.Play.SETG)
+            {
+                Console.WriteLine(setg);
+            }
+        }
+
+        private void dgvSETG_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            playbookFormRef.SetPlay(playbookFormRef.Play);
+            playbookFormRef.Play = playbookFormRef.GetPlay();
+            playbookFormRef.UpdatePlay(playbookFormRef.Play);
+            setSETG();
+
+            Program.ResizeDataGrid(dgvSETG);
+            Focus();
+        }
+
+        private void dgvSETP_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            foreach (PSALroute poso in playbookFormRef.Play.posos)
+            {
+                poso.Highlighted = false;
+            }
+
+            playbookFormRef.Play.posos[dgvSETP.CurrentRow.Index].Highlighted = true;
+
+            playbookFormRef.ClearField();
+            playbookFormRef.DrawPlayartPSAL(Graphics.FromImage(playbookFormRef.gImage), playbookFormRef.Play);
+        }
+
+        private void dgvSETG_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            foreach (PSALroute poso in playbookFormRef.Play.posos)
+            {
+                poso.Highlighted = false;
+            }
+
+            playbookFormRef.Play.posos[dgvSETG.CurrentRow.Index].Highlighted = true;
+
+            playbookFormRef.ClearField();
+            playbookFormRef.DrawPlayartPSAL(Graphics.FromImage(playbookFormRef.gImage), playbookFormRef.Play);
+        }
+
+        private void editSETGadd(object sender, EventArgs e)
+        {
+            int SGF_ = ((SGFF)playbookFormRef.cbxAlignments.SelectedItem).SGF_;
+
+            SETG setg = new SETG();
+
+            setg.rec = playbookFormRef.SETG.Count;
+            setg.SETP = playbookFormRef.Play.SETG[dgvSETG.CurrentCell.RowIndex].SETP;
+            setg.SGF_ = SGF_;
+            setg.SF__ = 0;
+            setg.x___ = playbookFormRef.Play.SETG[dgvSETG.CurrentCell.RowIndex].x___;
+            setg.y___ = playbookFormRef.Play.SETG[dgvSETG.CurrentCell.RowIndex].y___;
+            setg.fx__ = playbookFormRef.Play.SETG[dgvSETG.CurrentCell.RowIndex].fx__;
+            setg.fy__ = playbookFormRef.Play.SETG[dgvSETG.CurrentCell.RowIndex].fy__;
+            setg.anm_ = playbookFormRef.Play.SETG[dgvSETG.CurrentCell.RowIndex].anm_;
+            setg.dir_ = playbookFormRef.Play.SETG[dgvSETG.CurrentCell.RowIndex].dir_;
+            setg.fanm = playbookFormRef.Play.SETG[dgvSETG.CurrentCell.RowIndex].fanm;
+            setg.fdir = playbookFormRef.Play.SETG[dgvSETG.CurrentCell.RowIndex].fdir;
+
+            playbookFormRef.SETG.Add(setg);
+
+            playbookFormRef.Play = playbookFormRef.GetPlay();
+            playbookFormRef.UpdatePlay(playbookFormRef.Play);
+            setSETG();
+        }
+
+        private void editSETGremove(object sender, EventArgs e)
+        {
+            int record = playbookFormRef.Play.SETG[dgvSETG.CurrentCell.RowIndex].rec;
+
+            for (int i = playbookFormRef.SETG.Count - 1; i > record; i--)
+            {
+                playbookFormRef.SETG[i].rec -= 1;
+            }
+
+            playbookFormRef.SETG.RemoveAt(record);
+
+            playbookFormRef.Play = playbookFormRef.GetPlay();
+            playbookFormRef.UpdatePlay(playbookFormRef.Play);
+            setSETG();
+        }
+
+        private void dgvSETG_CellMouseUp(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.ColumnIndex < 0 || e.RowIndex < 0) return;
+            dgvSETG.CurrentCell = dgvSETG[e.ColumnIndex, e.RowIndex];
+
+            if (e.Button == MouseButtons.Right && e.ColumnIndex >= 0 && e.RowIndex >= 0)
+            {
+                if (((SGFF)playbookFormRef.cbxAlignments.SelectedItem).name == "Norm")
+                {
+                    cmsAddPlayerToMotion.Enabled = false;
+                    cmsRemovePlayerFromMotion.Enabled = false;
+                }
+                else
+                {
+                    if (playbookFormRef.Play.SETG[e.RowIndex].active)
+                    {
+                        cmsAddPlayerToMotion.Enabled = false;
+                        cmsRemovePlayerFromMotion.Enabled = true;
+                    }
+                    else
+                    {
+                        cmsAddPlayerToMotion.Enabled = true;
+                        cmsRemovePlayerFromMotion.Enabled = false;
+                    }
+                }
+
+                cmsEditSETG.Show(Cursor.Position.X, Cursor.Position.Y);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,97 @@
+# M20PlaybookEditor
+
+This playbook editor currently only supports custom playbooks.  It is in beta state with limited functionality, so bugs are possible, but it is relatively stable.
+
+There is limited undo ability and no Save As feature, so it would be a good idea to create backups before editing.
+
+
+Playbook Editor
+-
+You need a customplaybooks.db file.  Use Frosty to export customplaybooks.db Menu>Legacy>common>database>playbooks>customplaybooks.
+Open a customplaybooks.DB file from the file menu and a dropdown will appear with a list of formations.  All formations are listed together and will draw correctly, but only Offensive plays are drawn and able to be edited and saved.
+
+Select a Formation to show Sub-Formations and list all plays in the Sub-Formation.  The play will be drawn along with a list of player assignments which can be edited.
+
+To edit assignments, enter data manually in the data grid, or right-click a row in the data grid to show a list of assignments to choose from.  The assignments can be reset while in the popup window.  Clicking outside of this window will commit changes which can not be reverted.
+
+Play data such as the play name can also be edited by clicking the button next to the play name, or by right-clicking the play list to reveal a new window.  These changes can also be reverted by closing the window or commited by clicking the Update button.
+
+
+Route Visualizer
+-
+The Route Visualizer tool exists in the tools menu and currently only works with the provided csv.  It can draw and save routes one at a time, or save more than one route by selecting multiple groups of ID steps and selecting save all from the menu.
+
+# v1.14 - Copy/Paste PSAL
+
+-
+Added copy and paste to PSAL Assignment Table.  Copies and pastes all 11 PLYS entries. 
+
+# v1.11 - Playart Update
+
+-
+Option Routes and Play-Action Blocking now draw correctly in ARTL Playart.
+
+# v1.10 - Playart Update, Edit Sub-Formation (SETL), Add/Remove PLPD and PLRD, Add/Remove SRFT
+
+Playart Update
+-
+Offensive Playart draws in proper order now.  I.E. Blocking, QB, Base Routes, Primary Receiver, Delay Route, Motion Route, then Running.
+Defensive Playart draws in proper order now.  I.E. Rush/Blitz, Deep Zone, Hook then Flat.
+
+Edit Sub-Formation (SETL)
+-
+Added the ability to edit SETL (Sub-Formation) name, MOTN, CLAS, SETT, SITT, SLF_ (determines the defensive set) and poso.
+
+Add/Remove PLPD and PLRD
+-
+Added the ability to add or remove PLPD/PLRD records in Edit Play, under Options menu.
+
+Add/Remove SRFT
+-
+Added the ability to add or remove SRFT records.
+
+# v1.09 - Edit and Create PSAL
+
+Edit PSAL
+-
+Added the ability to edit existing PSALs.  Right-Click on the Player Assignment Table and select Swap/Edit PSAL.  A new window will open where new PSALs can be assigned.  Click the Edit checkbox to edit the current PSAL.  Right-Click a cell to Insert/Delete steps.  (WARNING - PSALs exist in multiple plays, so edit with caution)
+
+Create PSAL
+-
+Added an option to create a new PSAL.  Select a Player Assignment in the Player Assignment Table to use as a base for the new PSAL.  Right-Click on the Player Assignment Table and select Create New PSAL.  A new Window will open with the steps from the default PSAL.  Right-Click a cell to Insert/Delete steps and/or manually edit the PSAL by entering data into the cells of the table.  When finished, click the Create button to create the new PSAL.  To create a PSAL without assigning it, uncheck the 'Assign PSAL to Selected poso' checkbox, or leave the box checked to create and assign the PSAL.  Unassigned newly created PSALs will show in the Unused PSALs PLRR list in the Swap/Edit PSAL window.  Click the Discard button to cancel and discard the new PSAL.
+
+# v1.08 - Devensive Playart Update
+
+Defensive Playart is 99% complete if not 100% accurate. (ARTL)
+
+Playart can be saved by right-clicking the playart.
+
+# v1.07 - Defensive Edits, Playart Option, Player Assignment List
+
+Defensive Edits
+-
+Added support to edit and save Defensive Assignments and Defensive Play edits to the db.  Edit Play now populates with SRFT table that can be edited and saved.
+
+Playart Option
+-
+Playart can be drawn with raw data or with the Playart from Menu>Tools>Playart.  Still a WIP but good enough to view Defense as the Defensive raw data is incomplete at the moment.
+
+Player Assignment List
+-
+Right-clicking the Player Assignment Table now populates with a Data Grid of the current assignment, along with a list of assignments to choose from.
+
+# v1.06 - Formation Alignments, Flip Play, Formation (Alignment and Position) Edits
+
+Formation Alignments
+-
+Added the Formation Alignments and motions.  They are listed in the Motion/Alignments dropdown.  This will update in the Playart preview.
+*** Routes will not flip as they do in game; i.e. flat routes
+
+Flip Play
+-
+Added the ability to flip the play
+
+Formation Edits
+-
+Added the ability to edit/save Formation Alignments (STEG) and Formation Positions (STEP).
+Motions and alignments can be edited/saved.  Pick the motion that you want to edit in the Motions/Alignments dropdown, then click the Edit Formation button.  The inactive records (player assignments) for that motion/alignment will be disabled.  The remaining active rows are the players that account for that motion/alignment.  For example, M1le is motion player 1 to the left.  When the play is flipped the active offset is fx__, fy__; x___, y___ when the play is not flipped.
